610ac8c5f18e94cbd1fc0763643baffa52d4674b
div0021 <div2wk@gmail.com>
2024-05-28 14:46:34 +0530

    update script

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "deployement" ? "https://presidio-backend-ucbp.onrender.com" : "http://localhost:5173",
+
     credentials:true
 }));
 
--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export async function db() {
     const dbUrl = process.env.MONGO_URL;
 
-    console.log("DBURL",dbUrl)
-
     try{
         await mongoose.connect(dbUrl);
         console.log("DB is connected...");
